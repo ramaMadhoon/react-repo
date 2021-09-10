caf19ee7d217250bc6ddf6216c577a3d4a179553
email <rama>
2021-09-10 16:49:08 +0300

    second app react

--- src/App.js
+++ src/App.js
@@ -1,35 +1,63 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
-import * as UserCommponent from "./react-assignment";
-import UserInfo from './post';
+
 function App() {
+  let conveartText = true;
+  const clickMe = () => {
+    if (conveartText) {
+      conveartText = false;
+      setText("Hello Palestine");
+    } else {
+      conveartText = true;
+      setText("Hello World!");
+    }
+  };
+  const [text, setText] = useState(`Hello World!`);
   return (
     <div className="App">
-      <UserCommponent.compo1 />
-
-      <hr />
-
-      <UserCommponent.compo2 />
-
-      <hr />
-
-      <UserCommponent.compo3 />
-
-      <hr />
-      <UserCommponent.compo4 />
-
-      <hr />
-
-      <UserCommponent.compo5 />
-
-      <hr />
-      <UserInfo />
-    
-     
+      <span onClick={clickMe}>{text}</span>
     </div>
   );
 }
-
-export default App; 
- 
-
+const InputChange = () => {
+  const [userName, setUserName] = useState("user name");
+  const [email, setEmail] = useState("example@gmail.com");
+  return (
+    <div className="App">
+      <p>{`User Name : ${userName}`}</p>
+      <input
+        onChange={(event) => setUserName(event.target.value)}
+        placeholder="User Name"
+      />
+      <p>{`Email : ${email}`}</p>
+      <input
+        onChange={(event) => setEmail(event.target.value)}
+        placeholder="example@gmail.com"
+      />
+    </div>
+  );
+};
+const RandomSentences = () => {
+  const [sentence, setArr] = useState("Change Sentence");
+  const onClickButton = () => {
+    const sentenceArr = [
+      "Hi everyone",
+      "Hello World!",
+      "How are you",
+      "what is your country؟",
+      "What are you studying",
+      "I hope you are fine",
+      "I love you all my friends",
+      "What is your favorite programming language?",
+      "Erorr 404",
+      "last sentence",
+    ];
+    setArr(sentenceArr[Math.floor(Math.random() * sentenceArr.length)]);
+  };
+  return (
+    <div className="App">
+      <button onClick={onClickButton}>{sentence}</button>
+    </div>
+  );
+};
+export { App, InputChange, RandomSentences };
